9affcb8a161aed0704bf218f4926462418b8538b
Sujin S <57178220+BornAsSlayer@users.noreply.github.com>
2024-12-23 15:18:52 +0530

    updated factory file structure for better organization

--- CreationalPatterns/FactoryMethod/obj/Debug/net9.0/FactoryMethod.AssemblyInfo.cs
+++ CreationalPatterns/FactoryMethod/obj/Debug/net9.0/FactoryMethod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FactoryMethod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31842bdd7c3f5ea422684860b59dc58d1a8db109")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea4757e0bbe2434d674f56d9300b5898c12eac3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FactoryMethod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FactoryMethod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
